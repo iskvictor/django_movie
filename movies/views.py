5c3084d40893fc6462253c5bc1858ae3d718c736
iskvictor <victor.isk83@gmail.com>
2021-11-06 01:28:06 +0200

    show all parent reviews

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import View
 from .models import Movie
 from django.views.generic import ListView, DetailView
+from .forms import ReviewForm
 
 
 class MoviesView(ListView):
@@ -15,3 +16,17 @@ class MovieDetailView(DetailView):
 
     model = Movie
     slug_field = "url"
+
+
+class AddReview(View):
+    "Отзывы"
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            new_review = form.save(commit=False)
+            if request.POST.get('parent', None):
+                new_review.parent_id = int(request.POST.get('parent'))
+            new_review.movie = movie
+            new_review.save()
+        return redirect(movie.get_absolute_url())
